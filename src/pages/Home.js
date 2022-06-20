88afc672bdcd2533146ee6fb9daf2bb7c36c7910
HamoHamoHamo <hamo222@naver.com>
2022-06-20 09:36:35 +0000

    Feat: Add display schedule on calendar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,42 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
+import { SCHEDULE } from "../utils/Firebase";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [dataList, setDataList] = useState([]);
+
+  useEffect(() => {
+    let list = []
+    let id = []
+    SCHEDULE.orderBy("modifiedDate", "desc").get().then((docs) => {
+      docs.forEach((doc) => {
+        if(doc.exists){
+          list.push(doc.data());
+          id.push(doc.id);
+        }
+      });
+      setLoading(true);
+      setDataList(list.reduce((acc0, data, idx) => {
+        
+        const res = Object.entries(data).reduce((acc, [key, val], i) => {
+          // console.log("KEY", key, "\nval", val, "\nacc", acc);
+          if(key === 'title' || key === 'date') {
+            acc = {
+              ...acc,
+              [key]: val
+            }
+          }
+          return acc;
+        }, {id: id[idx]})
+        return acc0.concat(res);
+      }, []));
+
+    })
+  }, []);
+  console.log("DATALIST", dataList);
   return (
     <div class="">
       <div class="clearfix"></div>
@@ -16,10 +49,12 @@ export default function Home() {
                 defaultView="dayGridMonth"
                 title={"test"}
                 plugins={[dayGridPlugin]}
-                events={[
-                  { title: "event 1", date: "2022-05-13" },
-                  { title: "event 2", date: "2022-05-14" },
-                ]}
+                events={dataList.map((data) => ({
+                  title: data.title,
+                  date: data.date,
+                  url: `/datas/schedule/${data.id}`,
+                  className: "calendarFontSize",
+                }))}
                 locales={allLocales}
                 locale="ko"
               />
